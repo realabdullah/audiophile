92415ffe5e5b54731c463ebb1bd54031ce6834ac
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-07-03 15:27:39 +0100

    (chore): project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  css: ["~/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/core/_variables.scss"; @import "@/assets/scss/utils/_mixins.scss";`,
+        },
+      },
+    },
+  },
+  devtools: { enabled: true },
+});
