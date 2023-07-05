2dec7d2b782d881307959b8eac6633fae699b65a
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-07-05 13:40:50 +0100

    sss

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,33 @@
 </script>
 
 <template>
-    <h1>AUDIO-PHILE</h1>
-    <BaseButton variant="solid" text="See prodUCT" />
-    <br>
-    <br>
-    <BaseButton variant="outline" text="See prodUCT" />
-    <br>
-    <br>
-    <BaseButton variant="link" text="Shop" />
-    <br>
-    <br>
-    <BaseText label="Shop" for="shop" placeholder="Insert your name" :error="false" />
-    <br>
-    <br>
-    <BaseRadio label="Shop" for="shop" />
-    <br>
-    <br>
-    <BaseCount />
+    <div>
+        <TheHeader />
+        <h1>AUDIO-PHILE</h1>
+        <BaseButton variant="solid" text="See prodUCT" />
+        <br>
+        <br>
+        <BaseButton variant="outline" text="See prodUCT" />
+        <br>
+        <br>
+        <BaseButton variant="link" text="Shop" />
+        <br>
+        <br>
+        <BaseText label="Shop" for="shop" placeholder="Insert your name" :error="false" />
+        <br>
+        <br>
+        <BaseRadio label="Shop" for="shop" />
+        <br>
+        <br>
+        <BaseCount />
+    </div>
 </template>
+
+<style lang="scss" scoped>
+div {
+    background-color: rgba(12, 12, 12, 0.89);
+    max-width: 110rem;
+    margin: 0 auto;
+}
+</style>
+
