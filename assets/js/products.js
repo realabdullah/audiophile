f61b98aade1cebc7f1eaf2297ce5b66f8353971c
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-07-06 15:52:46 +0100

    (feat): products card and data

---
+++ assets/js/products.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    name: "XX99 Mark II Headphones",
+    description:
+      "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+    image:
+      "https://res.cloudinary.com/dxvhsze0l/image/upload/v1688646360/audiophile/products/bstkc6wsqtlhhmbxpy3c.png",
+    isNew: true,
+    category: "headphones",
+  },
+  {
+    name: "XX99 Mark I Headphones",
+    description:
+      "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
+    image:
+      "https://res.cloudinary.com/dxvhsze0l/image/upload/v1688646361/audiophile/products/hjqrx9siy155rteimdtd.png",
+    isNew: false,
+    category: "headphones",
+  },
+  {
+    name: "XX59 Headphones",
+    description:
+      "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.",
+    image:
+      "https://res.cloudinary.com/dxvhsze0l/image/upload/v1688646359/audiophile/products/jo1lt9wmmmjjrjcclnpi.png",
+    isNew: false,
+    category: "headphones",
+  },
+  {
+    name: "ZX9 SPEAKER",
+    description:
+      "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+    image:
+      "https://res.cloudinary.com/dxvhsze0l/image/upload/v1688648202/audiophile/products/yzqbg76hc1earixs1sbn.png",
+    isNew: true,
+    category: "speakers",
+  },
+  {
+    name: "ZX7 SPEAKER",
+    description:
+      "Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
+    image:
+      "https://res.cloudinary.com/dxvhsze0l/image/upload/v1688648202/audiophile/products/iiidlu5qfewan64jw3fa.png",
+    isNew: false,
+    category: "speakers",
+  },
+  {
+    name: "YX1 WIRELESS EARPHONES",
+    description:
+      "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.",
+    image:
+      "https://res.cloudinary.com/dxvhsze0l/image/upload/v1688648497/audiophile/products/b9uswnre2wymtjiexpcb.png",
+    isNew: true,
+    category: "earphones",
+  },
+];
---
+++ components/ProductCards.vue
@@ -0,0 +1,115 @@
+<script lang="ts" setup>
+interface Product {
+    name: string;
+    description: string;
+    image: string;
+    isNew: boolean;
+}
+
+interface Products {
+    products: Product[];
+}
+
+defineProps<Products>();
+
+const isEven = (index: number) => index % 2 === 0;
+</script>
+
+<template>
+    <div v-if="products && products.length > 0" class="wrapper">
+        <div v-for="(product, index) in products" class="product d-flex align-items-center justify-content-space-between"
+            :class="{ 'flex-row-reverse': !isEven(index) }">
+            <div class="product__image w-100 d-flex align-items-center justify-content-center">
+                <img :src="product.image" :alt="product.name">
+            </div>
+            <div class="product__info d-flex flex-column align-items-start">
+                <span v-if="product.isNew" class="new weight-400">NEW PRODUCT</span>
+                <h2 class="text-transform-uppercase">{{ product.name }}</h2>
+                <p>{{ product.description }}</p>
+
+                <BaseButton variant="solid" text="See Product" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+    max-width: 110rem;
+    margin: 16rem auto;
+    padding: 0 3rem;
+
+    @media screen and (max-width: 768px) {
+        margin: 12rem auto;
+    }
+
+    .product {
+        gap: 5rem;
+        margin-bottom: 16rem;
+
+        &:last-child {
+            margin-bottom: 0;
+        }
+
+        @media screen and (max-width: 768px) {
+            flex-direction: column !important;
+        }
+
+        &__image {
+            max-width: 50rem;
+            height: 56rem;
+            border-radius: 0.8rem;
+            background-color: $col-silver;
+            padding: 7rem;
+
+            @media screen and (max-width: 768px) {
+                max-width: 100%;
+                height: 35rem;
+            }
+
+            img {
+                width: 34rem;
+                height: 38rem;
+                object-fit: contain;
+                filter: drop-shadow(0px 49px 19px #CAB7B7);
+
+                @media screen and (max-width: 768px) {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+
+        &__info {
+
+            @media screen and (max-width: 768px) {
+                align-items: center !important;
+                text-align: center;
+            }
+
+            .new {
+                @include font(1.4rem, 1rem);
+                letter-spacing: 1rem;
+                color: $col-burntSienna;
+            }
+
+            h2 {
+                max-width: 30rem;
+                @include font(4rem, 4.4rem);
+                letter-spacing: 0.14rem;
+                color: $col-black;
+                margin-top: 1.6rem;
+                margin-bottom: 3.2rem;
+            }
+
+            p {
+                max-width: 44rem;
+                @include font(1.5rem, 2.5rem);
+                color: $col-black;
+                opacity: 0.5;
+                margin-bottom: 4rem;
+            }
+        }
+    }
+}
+</style>
